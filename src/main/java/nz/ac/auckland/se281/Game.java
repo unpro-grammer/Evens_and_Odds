aae58197214c80064bbdbbe929eab86abb15f0bc
Grace <gxu660@aucklanduni.ac.nz>
2024-05-11 20:32:25 +1200

    Print greeting message after player enters name

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,9 +5,17 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
-
+  /**
+   * Registers the desired difficulty level and choice (odd or even), and then greets the user based
+   * on the name they give.
+   *
+   * @param difficulty
+   * @param choice
+   * @param options
+   */
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
   public void play() {}
