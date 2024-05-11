05ddc64cda68965b30a1a1475e14432c65aae6f5
Grace <gxu660@aucklanduni.ac.nz>
2024-05-11 21:18:38 +1200

    Add helper class for execution of game strategy for bot

---
+++ src/main/java/nz/ac/auckland/se281/StrategyExecution.java
@@ -0,0 +1,10 @@
+package nz.ac.auckland.se281;
+
+public class StrategyExecution {
+
+  private GameStrategy gameStrat;
+
+  public StrategyExecution(GameStrategy gameStrat) {
+    this.gameStrat = gameStrat;
+  }
+}
