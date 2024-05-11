71d3c921436ece077306abb91145c19468946eec
Grace <gxu660@aucklanduni.ac.nz>
2024-05-11 22:02:26 +1200

    Add method to set strategy for AI

--- src/main/java/nz/ac/auckland/se281/ArtificialIntelligence.java
+++ src/main/java/nz/ac/auckland/se281/ArtificialIntelligence.java
@@ -4,10 +4,14 @@ import nz.ac.auckland.se281.Main.Choice;
 
 public abstract class ArtificialIntelligence {
   protected Choice choice;
-  protected GameStrategy gamestrat;
+  protected GameStrategy gameStrat;
 
   public ArtificialIntelligence(Choice choice) {
     this.choice = choice;
+  }
+
+  public void setStrategy(GameStrategy gameStrat) {
+    this.gameStrat = gameStrat;
   }
 
   public abstract void play();
