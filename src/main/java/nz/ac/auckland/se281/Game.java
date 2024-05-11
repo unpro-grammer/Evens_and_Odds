a470b73e97b9f8637f73a673bf44f252a577d1a2
Grace <gxu660@aucklanduni.ac.nz>
2024-05-11 20:41:03 +1200

    Implementer counter to track round of game and display this each round

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,20 +5,26 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+
+  int round = 0;
+
   /**
    * Registers the desired difficulty level and choice (odd or even), and then greets the user based
    * on the name they give.
    *
-   * @param difficulty
-   * @param choice
-   * @param options
+   * @param difficulty difficulty level between easy, medium, and hard
+   * @param choice player's wager between odd and even
+   * @param options array containing user's name
    */
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    round++;
+    MessageCli.START_ROUND.printMessage(round + "");
+  }
 
   public void endGame() {}
 
