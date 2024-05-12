0b2b27aa857d7ad5a133d3c4fa7cc560b1b6de80
Grace <gxu660@aucklanduni.ac.nz>
2024-05-12 20:54:23 +1200

    Refactor code in RandomStrategy to reduce code quantity

--- src/main/java/nz/ac/auckland/se281/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -4,11 +4,8 @@ import nz.ac.auckland.se281.Main.Choice;
 
 public class RandomStrategy implements GameStrategy {
 
-  int aiHand;
-
   @Override
   public int playNumber(int[] history, Choice choice) {
-    aiHand = Utils.getRandomNumberRange(0, 5);
-    return aiHand;
+    return Utils.getRandomNumberRange(0, 5);
   }
 }
